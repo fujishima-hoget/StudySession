156bf3c3bbabce4faac243e39887e25a5729c62b
PC-024\PC-02 <fujishima.shota@hoget.co.jp>
2025-10-31 19:48:26 +0900

    Add : ちょっとだけスクリプトとかマテリアル追加

--- ForStudySesssion/Assets/Scripts/Node/Node.cs
+++ ForStudySesssion/Assets/Scripts/Node/Node.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class TileStatus : MonoBehaviour
+public class Node : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
